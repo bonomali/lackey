4e617d47e827fc51b286c5f207b40525338c02b0
Nejc Habjan <hab.nejc@gmail.com>
2017-11-04 23:52:17 +0100

    pytest: fix decorators

--- tests/appveyor_test_cases.py
+++ tests/appveyor_test_cases.py
@@ -10,32 +10,32 @@ import lackey
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture()
 def kb():
     return lackey.Keyboard()
 
 
-@pytest.fixture
+@pytest.fixture()
 def mouse():
     return lackey.Mouse()
 
 
-@pytest.fixture
+@pytest.fixture()
 def screen():
     return lackey.Screen(0)
 
 
-@pytest.fixture
+@pytest.fixture()
 def test_loc():
     return lackey.Location(10, 11)
 
 
-@pytest.fixture
+@pytest.fixture()
 def pattern_path():
     return os.path.join("tests", "test_pattern.png")
 
 
-@pytest.fixture
+@pytest.fixture()
 def pattern(pattern_path):
     print(pattern_path)
     return lackey.Pattern(pattern_path)
